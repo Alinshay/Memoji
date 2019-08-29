1122fad29e72ed4762b2511e2a317a9d3272d3d5
Alinshay <alinashay@icloud.com>
2019-08-29 13:53:10 +0300

    logic part 1

--- script.js
+++ script.js
@@ -1,18 +1,87 @@
 
 
-function random()
-{
-    var mas = ['🐭' ,'🐭', '🐸','🐸','🐱','🐱','🐷','🐷','🐶','🐶','🐵','🐵'];
+function random() {
+    var mas = ['🐭', '🐭', '🐸', '🐸', '🐱', '🐱', '🐷', '🐷', '🐶', '🐶', '🐵', '🐵'];
 
-    mas =mas.sort(function(){
+    mas = mas.sort(function () {
         return Math.random() - 0.5;
     });
 
     var collection = document.querySelectorAll('.back ');
-    for(var i=0; i<collection.length; i++)
+    for (var i = 0; i < collection.length; i++)
         collection[i].innerHTML = mas[i];
+}
 
-    alert(collection.length);
+function logic(){
+
+    var card = document.querySelectorAll('.front ');
+    for(var i=0; i<card.length; i++) {
+
+        card[i].addEventListener('click', click, true);
+    }
+
+
+
+    function click()
+    {
+        var backs = document.querySelectorAll('.back ');
+
+        //поменять на что-то более норм
+        var op=0;
+        top:
+        for(var i=0; i<backs.length; i++)  {
+
+            if (backs[i].dataset.state == 'open')
+            {
+                op = 1;
+
+             if (backs[i].innerHTML == this.nextElementSibling.innerHTML)
+            {
+                this.nextElementSibling.dataset.state = 'green';
+                this.previousElementSibling.setAttribute('checked', true);
+                this.previousElementSibling.setAttribute('disabled', true);
+
+                backs[i].dataset.state = 'green';
+                backs[i].previousElementSibling.previousElementSibling.setAttribute('checked', true);
+                backs[i].previousElementSibling.previousElementSibling.setAttribute('disabled', true);
+
+
+                //disabled checked
+                //блокируются
+                break top;
+            }
+
+            else
+            {
+                this.nextElementSibling.dataset.state = 'red';
+                backs[i].dataset.state = 'red';
+
+                backs[i].previousElementSibling.previousElementSibling.setAttribute('checked', true);
+                backs[i].previousElementSibling.previousElementSibling.setAttribute('disabled', false);
+                this.previousElementSibling.setAttribute('checked', true);
+                this.previousElementSibling.setAttribute('disabled', false);
+
+                //переварачиваются обратно сами
+                break top;
+
+            }
+                }
+
+            }
+
+        if(op==0) {
+            this.nextElementSibling.dataset.state = 'open';
+            this.previousElementSibling.setAttribute('checked', true);
+            this.previousElementSibling.setAttribute('disabled', true);
+
+        }
+
+
+    }
 
 
 }
+
+//TODO
+//запретить закрывание обратно
+//закрывать самим

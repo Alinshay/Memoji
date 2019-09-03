d6950f7d27cfecbdbb82d5fc6ccb7b874c1f6ab4
Alinshay <alinashay@icloud.com>
2019-09-03 13:08:45 +0300

    edits

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ function random() {
         collection[i].innerHTML = mas[i];
 }
 
+
+
 function logic(){
 
     var card = document.querySelectorAll('.front ');
@@ -22,66 +24,102 @@ function logic(){
 
 
 
+
     function click()
     {
         var backs = document.querySelectorAll('.back ');
+        //проверка не все ли зеленые?
+
 
         //поменять на что-то более норм
         var op=0;
         top:
-        for(var i=0; i<backs.length; i++)  {
+            for(var i=0; i<backs.length; i++)  {
 
-            if (backs[i].dataset.state == 'open')
-            {
-                op = 1;
+                if (backs[i].dataset.state == 'open')
+                {
+                    op = 1;
 
-             if (backs[i].innerHTML == this.nextElementSibling.innerHTML)
-            {
-                this.nextElementSibling.dataset.state = 'green';
-                this.previousElementSibling.setAttribute('checked', true);
-                this.previousElementSibling.setAttribute('disabled', true);
+                    if (backs[i].innerHTML == this.nextElementSibling.innerHTML)
+                    {
+                        this.nextElementSibling.dataset.state = 'green';
+                        //this.previousElementSibling.setAttribute('checked', true);
+                        //this.previousElementSibling.setAttribute('disabled', true);
 
-                backs[i].dataset.state = 'green';
-                backs[i].previousElementSibling.previousElementSibling.setAttribute('checked', true);
-                backs[i].previousElementSibling.previousElementSibling.setAttribute('disabled', true);
+                        backs[i].dataset.state = 'green';
+                        //backs[i].previousElementSibling.previousElementSibling.setAttribute('checked', true);
+                        //backs[i].previousElementSibling.previousElementSibling.setAttribute('disabled', true);
 
 
-                //disabled checked
-                //блокируются
-                break top;
-            }
+                        //disabled checked
+                        //блокируются
 
-            else
-            {
-                this.nextElementSibling.dataset.state = 'red';
-                backs[i].dataset.state = 'red';
+                    }
 
-                backs[i].previousElementSibling.previousElementSibling.setAttribute('checked', true);
-                backs[i].previousElementSibling.previousElementSibling.setAttribute('disabled', false);
-                this.previousElementSibling.setAttribute('checked', true);
-                this.previousElementSibling.setAttribute('disabled', false);
+                    else
+                    {
+                        this.nextElementSibling.dataset.state = 'red';
+                        backs[i].dataset.state = 'red';
 
-                //переварачиваются обратно сами
-                break top;
+                        //backs[i].previousElementSibling.previousElementSibling.setAttribute('checked', true);
+                        //backs[i].previousElementSibling.previousElementSibling.setAttribute('disabled', false);
+                        //this.previousElementSibling.setAttribute('checked', true);
+                        //this.previousElementSibling.setAttribute('disabled', false);
 
-            }
+                        break top;
+
+                    }
                 }
 
+
             }
 
-        if(op==0) {
+
+
+
+                        if(op==0) {
             this.nextElementSibling.dataset.state = 'open';
-            this.previousElementSibling.setAttribute('checked', true);
-            this.previousElementSibling.setAttribute('disabled', true);
+            //this.previousElementSibling.setAttribute('checked', true);
+            //this.previousElementSibling.setAttribute('disabled', true);
+
+            var red1; var red2;
+            for(var i=0; i<backs.length; i++)
+            {
+                if(backs[i].dataset.state == 'red') {
+                    if (red1 == undefined)
+                        red1 = backs[i].previousElementSibling.previousElementSibling.getAttribute('id');
+
+                    else red2 = backs[i].previousElementSibling.previousElementSibling.getAttribute('id');
+
+                    backs[i].dataset.state = 'close';
+                }
+            }
+
+            document.getElementById(red1).click();
+            document.getElementById(red2).click();
+
 
         }
 
 
     }
 
+    /*
+    var green =0;
+        for(var i=0; i<backs.length; i++) {
+            if (backs[i].dataset.state!= 'green')
+                green+=1;
+        }
+        if(green==0)
+            alert('ура!');
+     */
+
+}
+
+function animation(){
+
 
 }
 
 //TODO
-//запретить закрывание обратно
 //закрывать самим
